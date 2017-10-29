48137391b5ed6a2cf4415e9a068b332b89c91b62
mzielinska <magda.zielinska@gmail.com>
2017-10-29 11:21:00 +0100

    poprawki do slidera

--- Warsztat_slider/js/slider.js
+++ Warsztat_slider/js/slider.js
@@ -6,8 +6,12 @@ $(document).ready(function () {
 	var slideWidth = 100 / slideCount;
 	var slideIndex = 0;
 
+	//szerokośc kontenera .slide-show
+	
 	slideShow.css("width", (slideCount * 100) + "%");
-
+	
+	// Nadaj kazdemu slajdowi szerokosc i lewy margines
+	
 	slideShow.find(".single-slide").each(function (index) {
 		console.log("działa");
 		$(this).css({
@@ -16,6 +20,8 @@ $(document).ready(function () {
 		});
 	});
 
+	//Obsługa nawigacji slajdera
+	
 	function slide(newSlideIndex) {
 		if (newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
 			console.log("nie ma slajdu o takim indexie");
@@ -41,12 +47,3 @@ $(document).ready(function () {
 		slide(slideIndex + 1);
 	})
 });
-
-//4.	Dla nawigacji (elementy z klasami .prev-slide i .next-slide) przypisz obsługę zdarzenia click. Sprawdź, czy obsługa zdarzenia click działa na tej nawigacji. Następnie wewnątrz funkcji obsługującej zdarzenie click wywołaj funkcję slide() (jej definicja jest w kolejnym punkcie), która przyjmuje parametr slideIndex – 1 jeśli przewijamy w lewo i slideIndex + 1 jeśli przewijamy w prawo.
-//5.	Zdefiniuj funkcję slide() przyjmującą parametr newSlideIndex, która odpowiada za przewijanie slajdów:
-//a.	Sprawdź, czy aktualny newSlideIndex nie jest mniejszy od indexu pierwszego slajdu lub większy od indexu ostatniego slajdu. Jeśli tak, to przerwij działanie funkcji
-//b.	Zdefiniuj zmienną slideCaption, do której przypisz aktualny napis na slajdzie (elementy z klasą .slide-caption)
-//c.	Stwórz zmienną marginLeft, która przyjmie wartość procentową (%) aktualnego indexu slidu pomnożoną przez -100 (o taką wartość marginesu przesuwamy cały rząd ze slajdami)
-//d.	Ukryj napis znajdujący się w zmiennej slideCaption używając metody .hide()
-//e.	Na kontenerze slideShow wykonaj animacje przesuwającą wszystkie slajdy w lewo lub w prawo poprzez ustawienie marginesu – margin-left: marginLeft, nadaj jej czas trwania 800 milisekund i wywołaj funkcję callback (jej opis poniżej)
-//f.	Funkcja anonimowa callback ma do zmiennej slideIndex przypisać nowy index slajdu ze zmienne newSlideIndex oraz wyświetlić napis na slidzie (zmienna slideCaption) za pomocą metody .fadeIn()
